3a3b0e4d7c1063e2dd593bdc3db4e76b3ff493d8
mastership <mastership.un@gmail.com>
2017-06-28 14:45:09 +0900

    second commit

---
+++ msLibTest/Classes/msLibTestLog.h
@@ -0,0 +1,15 @@
+//
+//  msLibTestLog.h
+//  Pods
+//
+//  Created by mastership on 2017. 6. 28..
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface msLibTestLog : NSObject
+
+- (void)Log;
+
+@end
---
+++ msLibTest/Classes/msLibTestLog.m
@@ -0,0 +1,18 @@
+//
+//  msLibTestLog.m
+//  Pods
+//
+//  Created by mastership on 2017. 6. 28..
+//
+//
+
+#import "msLibTestLog.h"
+
+@implementation msLibTestLog
+
+- (void)Log
+{
+    NSLog(@"!!!!!!!!!!!!!!!!!!");
+}
+
+@end
